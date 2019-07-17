e110e26219785b78701c89224dac5180cd776b4d
Saminu <s.salisu@live.co.uk>
2019-07-17 15:17:10 +0200

    update to site

--- js/main.js
+++ js/main.js
@@ -126,3 +126,14 @@ if($().circleProgress){
 }
 
 })(jQuery);
+
+function documentReady(callback) {
+	// in case the document is already rendered
+	if (document.readyState != 'loading') callback();
+	// modern browsers
+	else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
+	// IE <= 8
+	else document.attachEvent('onreadystatechange', function () {
+			if (document.readyState == 'complete') callback();
+		});
+}
